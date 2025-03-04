53dfcf30cd8e33ce0caeffbca379002be71517b2
Anais <anais.carmon@educa.jcyl.es>
2025-03-04 17:56:16 +0100

    Anais (#12)
    * Eliminando el js para crear conflictos
    
    * No me lo cargo, solo lo cambio un poquito
    
    * Intentando liarla
    
    * Conflicto bulla jaleo

--- javascript/mijava.js
+++ javascript/mijava.js
@@ -21,4 +21,7 @@ closebtn.addEventListener("click", () => {
     popup.style.display = "none";
     showbtn.style.display = "block";
     // document.body.style.backgroundColor = "#09dbd450";
+    subp.style.display = "none";
+
+});
 });
